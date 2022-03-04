b50ed2c38a8e21b88a4e82d8007b2dc70ffc3a5d
franciskershawfc <99820703+franciskershawfc@users.noreply.github.com>
2022-03-04 14:19:40 +0000

    finis off second walkthrough project

--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -4,18 +4,25 @@ const githubReducer = (state, action) => {
             return {
                 ...state,
                 users: action.payload,
-                loading: false
-            }
+                loading: false,
+            };
+        case 'GET_USER_AND_REPOS':
+            return {
+                ...state,
+                user: action.payload.user,
+                repos: action.payload.repos,
+                loading: false,
+            };
         case 'SET_LOADING':
             return {
                 ...state,
-                loading: true
-            }
+                loading: true,
+            };
         case 'CLEAR_USERS':
             return {
                 ...state,
-                users: []
-            }
+                users: [],
+            };
         default:
             return state;
     }
